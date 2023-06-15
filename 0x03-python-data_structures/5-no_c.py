364228e66ca5717c1ffcf3bb9c62e517112203da
gabbyedgar <ochibaedgar@gmail.com>
2023-06-14 19:04:12 -0700

    python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = []
-    for alpha in my_string:
-        if alpha != 'c' and letter != 'C':
-            result.append(letter)
-    return ''.join(result)
+    s = f""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            s += f"{i}"
+    return s
