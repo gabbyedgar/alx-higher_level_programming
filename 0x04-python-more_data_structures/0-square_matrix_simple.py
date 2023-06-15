5e0ef409723feb95edceee20d6f0b60a586c77e3
gabbyedgar <ochibaedgar@gmail.com>
2023-06-14 18:03:35 -0700

    python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    for i in range(len(new_matrix)):
+        new_matrix[i] = list(map(lambda x: x ** 2, new_matrix[i]))
+    return new_matrix
