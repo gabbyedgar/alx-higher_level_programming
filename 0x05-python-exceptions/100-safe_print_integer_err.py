de83e6237cfa0b9baa0e2a9257bc3b2ae28cf9cd
Ochiteck <122891682+gabbyedgar@users.noreply.github.com>
2023-06-27 01:43:17 +0100

    Python

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        return (False)
