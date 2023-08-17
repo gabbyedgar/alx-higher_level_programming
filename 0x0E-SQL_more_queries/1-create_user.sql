2134fc18dfe3fd33de2889353c9a0a9c4cb74ce7
ocheteck <ochibaedgar@gmail.com>
2023-08-17 07:56:35 -0700

    edit task 3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Create database `hbtn_0d_2` and user `user_0d_2`
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.*
+      TO 'user_0d_2'@'localhost'
+      IDENTIFIED BY 'user_0d_2_pwd';
